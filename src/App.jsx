bbb82da5c9e57c33726f642de7e3dae1e6500c54
Jayesh Sharma <unravellabs@gmail.com>
2026-03-16 12:52:24 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,42 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { AuthProvider, useAuth } from './lib/useAuth'
-import { useTheme } from './lib/useTheme'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from './hooks/useAuth'
+import { ThemeProvider } from './hooks/useTheme'
 import Layout from './components/Layout'
 import Home from './pages/Home'
-import SubjectPage from './pages/SubjectPage'
+import Browse from './pages/Browse'
 import QuizMode from './pages/QuizMode'
 import CustomTest from './pages/CustomTest'
-import TestRunner from './pages/TestRunner'
 import Analytics from './pages/Analytics'
 import Bookmarks from './pages/Bookmarks'
-import YearPage from './pages/YearPage'
-
-function AppRoutes() {
-  const { dark } = useTheme()
-  return (
-    <Layout dark={dark}>
-      <Routes>
-        <Route path="/"               element={<Home dark={dark} />} />
-        <Route path="/subject/:id"    element={<SubjectPage dark={dark} />} />
-        <Route path="/year/:year"     element={<YearPage dark={dark} />} />
-        <Route path="/quiz"           element={<QuizMode dark={dark} />} />
-        <Route path="/custom"         element={<CustomTest dark={dark} />} />
-        <Route path="/test/run"       element={<TestRunner dark={dark} />} />
-        <Route path="/analytics"      element={<Analytics dark={dark} />} />
-        <Route path="/bookmarks"      element={<Bookmarks dark={dark} />} />
-      </Routes>
-    </Layout>
-  )
-}
+import YearAnalysis from './pages/YearAnalysis'
+import QuestionDetail from './pages/QuestionDetail'
+import TestResult from './pages/TestResult'
 
 export default function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="browse" element={<Browse />} />
+              <Route path="browse/:subject" element={<Browse />} />
+              <Route path="browse/:subject/:chapter" element={<Browse />} />
+              <Route path="question/:id" element={<QuestionDetail />} />
+              <Route path="quiz" element={<QuizMode />} />
+              <Route path="custom-test" element={<CustomTest />} />
+              <Route path="test-result/:testId" element={<TestResult />} />
+              <Route path="analytics" element={<Analytics />} />
+              <Route path="bookmarks" element={<Bookmarks />} />
+              <Route path="years" element={<YearAnalysis />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
   )
 }
+
+
